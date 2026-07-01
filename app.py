6ad0e655718914ff0eb589911591d9ad49643aa6
ANNAMDEVULA DURGA PRASAD <durgaprasadannamdevula41@gmail.com>
2026-07-01 17:20:21 +0530

    Fix formatting issue in app.py

--- app.py
+++ app.py
@@ -51,7 +51,6 @@ st.write("""
 **Version Control:** GitHub
 
 **Purpose:** Help telecom companies identify customers who are likely to churn and support customer retention strategies.
-""")
 
 st.sidebar.title("Customer Details")
 
