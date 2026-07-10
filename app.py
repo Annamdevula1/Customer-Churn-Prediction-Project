abbfe5931663b9c31fa3117c909ed6ca719dc1e0
ANNAMDEVULA DURGA PRASAD <durgaprasadannamdevula41@gmail.com>
2026-07-10 17:14:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -85,7 +85,7 @@ st.markdown("""
 # Header
 # ==========================================
 
-st.image("project image.jpeg", use_container_width=True)
+st.image("project image.jpeg", width="stretch")
 
 st.markdown("""
 <div class="header-box">
@@ -567,12 +567,12 @@ for i in range(0, len(images), 2):
     col1, col2 = st.columns(2)
 
     with col1:
-        st.image(images[i],use_container_width=True)
+        st.image(images[i],width="stretch")
 
     if i + 1 < len(images):
 
         with col2:
-            st.image(images[i + 1],use_container_width=True)
+            st.image(images[i + 1],width="stretch")
 
 # ==========================================
 # Developer Section
